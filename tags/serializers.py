607dc932b503c89565e725f0e65796a4eedb29f0
vamsigitlab <vamsiparupalli1@gmail.com>
2023-08-05 17:40:58 +0530

    Tag serializer is defined

---
+++ tags/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from tags.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug', 'created_at']
