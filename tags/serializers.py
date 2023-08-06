662002da51380bcb4e2119b33560287b40c61a93
vamsigitlab <vamsiparupalli1@gmail.com>
2023-08-06 13:07:34 +0530

    Learned the second use case of serializer

--- tags/serializers.py
+++ tags/serializers.py
@@ -3,6 +3,10 @@ from tags.models import Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
+
+    #Formating some fileds which are present in the models
+
+    created_at = serializers.DateTimeField(format="%d/%m/%y")
     class Meta:
         model = Tag
         fields = ['id', 'name', 'slug', 'created_at']
